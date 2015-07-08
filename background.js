a4c34cb25e35cf19261c35f9e2081ae1579e46e1
Travis Paul <Tr@visPaul.me>
2015-07-07 22:51:22 -0400

    Navigate to http://www.pkgsrc.org/is-a-freeze-on/ on click

--- background.js
+++ background.js
@@ -2,48 +2,50 @@
 
     'use strict';
 
-    var check = function (alarm, method) {
-        var xhr = new XMLHttpRequest()
-            method = method || 'HEAD';
+    var 
+        url = 'http://www.pkgsrc.org/is-a-freeze-on/',
+        check = function (alarm, method) {
+            var xhr = new XMLHttpRequest()
+                method = method || 'HEAD';
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                var etag = xhr.getResponseHeader('etag'),
-                    last_etag = localStorage.getItem('etag');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    var etag = xhr.getResponseHeader('etag'),
+                        last_etag = localStorage.getItem('etag');
 
-                if (method === 'GET') {
-                    localStorage.setItem('etag', etag);
-                    checkStatus(xhr.responseText);
-                } else if (etag !== last_etag) {
-                    check(null, 'GET');
+                    if (method === 'GET') {
+                        localStorage.setItem('etag', etag);
+                        checkStatus(xhr.responseText);
+                    } else if (etag !== last_etag) {
+                        check(null, 'GET');
+                    }
                 }
             }
-        }
 
-        xhr.open(method, 'http://www.pkgsrc.org/is-a-freeze-on/');
-        xhr.send();
-    },
-    checkStatus = function (responseText) {
-        var doc = document.implementation.createHTMLDocument(),
-            status;
+            xhr.open(method, url);
+            xhr.send();
+        },
+        checkStatus = function (responseText) {
+            var doc = document.implementation.createHTMLDocument(),
+                status;
 
-        doc.documentElement.innerHTML = responseText;
-        status = doc.querySelector('#content > h1').innerText;
+            doc.documentElement.innerHTML = responseText;
+            status = doc.querySelector('#content > h1').innerText;
 
-        if (status.toLowerCase() === 'yes') {
-            chrome.browserAction.setIcon({path: '19-freeze.png'});
-            chrome.browserAction.setTitle({title:
-                chrome.i18n.getMessage('status_freeze')});
-        } else if (status.toLowerCase() === 'no') {
-            chrome.browserAction.setIcon({path: '19-pkgsrc.png'});
-            chrome.browserAction.setTitle({title:
-                chrome.i18n.getMessage('status_ok')});
-        } else {
-            chrome.browserAction.setIcon({path: '19-default.png'});
-            chrome.browserAction.setTitle({title:
-                chrome.i18n.getMessage('status_unknown')});
-        }
-    };
+            if (status.toLowerCase() === 'yes') {
+                chrome.browserAction.setIcon({path: '19-freeze.png'});
+                chrome.browserAction.setTitle({title:
+                    chrome.i18n.getMessage('status_freeze')});
+            } else if (status.toLowerCase() === 'no') {
+                chrome.browserAction.setIcon({path: '19-pkgsrc.png'});
+                chrome.browserAction.setTitle({title:
+                    chrome.i18n.getMessage('status_ok')});
+            } else {
+                chrome.browserAction.setIcon({path: '19-default.png'});
+                chrome.browserAction.setTitle({title:
+                    chrome.i18n.getMessage('status_unknown')});
+            }
+        };
 
     chrome.alarms.get('check-freeze', function (alarm) {
         if (!alarm) {
@@ -63,6 +65,9 @@
         window.localStorage.removeItem('etag');
     });
 
+    chrome.browserAction.onClicked.addListener(function () {
+        chrome.tabs.create({url:url});
+    });
 
     check();
 
